c47c8c922bba56c52500c6c9369caad9cc7b307a
angelgm1729 <angel.granado@correo.unimet.edu.ve>
2023-06-10 03:01:44 -0400

    Not Found Page :white_check_mark:

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,9 +1,20 @@
 import styles from "./NotFoundPage.module.css";
+import { Link } from "react-router-dom";
+import { HOME_URL } from "../../constants/urls";
 
 export function NotFoundPage() {
     return (
         <div className={styles.container}>
             <h1 className={styles.title}>404 NOT FOUND!</h1>
+            <p className={styles.description}>
+                Lo sentimos, la página que estás buscando no existe.
+            </p>
+            <p className={styles.redirectText}>
+                ¿Deseas volver a la página de inicio?
+            </p>
+            <Link to={HOME_URL} className={styles.redirectLink}>
+                Volver a la página de inicio
+            </Link>
         </div>
     );
 }
