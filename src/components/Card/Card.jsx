9ebb9b6167dd0964c4719c75f1e2261967566805
André Scarcia <andre.scarcia@correo.unimet.edu.ve>
2023-06-11 01:48:22 -0400

    lo logre! :+1:

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,32 +4,34 @@ import { Link } from "react-router-dom";
 import { MOVIE_DETAIL_URL } from "../../constants/urls";
 
 export function Card({ movie }) {
-    return (
-        <div className={styles.container} onClick={MOVIE_DETAIL_URL(movie.id)}>
-            <div>
-                <img
-                    src={movie.poster_path}
-                    alt={movie.title}
-                    className={styles.image}
-                />
-            </div>
-            <div className={styles.rightSideContainer}>
-                <div className={styles.infoContainer}>
-                    <h2 className={styles.title}>
-                        <Link
-                            to={MOVIE_DETAIL_URL(movie.id)}
-                            className={styles.link}
-                        >
-                            {movie.title}
-                        </Link>
-                    </h2>
-                    <div className={styles.genreRow}>
-                        <h3 className={styles.genre}>{movie.genre}</h3>
-                        <h3 className={styles.language}>{movie.language}</h3>
-                    </div>
-                </div>
-            </div>
+  const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`; // Agrega la URL base de la imagen
+
+  return (
+    <div className={styles.container} onClick={MOVIE_DETAIL_URL(movie.id)}>
+      <div>
+        <img
+          src={imageUrl} // Utiliza la URL completa de la imagen
+          alt={movie.title}
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.rightSideContainer}>
+        <div className={styles.infoContainer}>
+          <h2 className={styles.title}>
+            <Link
+              to={MOVIE_DETAIL_URL(movie.id)}
+              className={styles.link}
+            >
+              {movie.title}
+            </Link>
+          </h2>
+          <div className={styles.genreRow}>
+            <h3 className={styles.genre}>{movie.genre}</h3>
+            <h3 className={styles.language}>{movie.language}</h3>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { ImageCarousel } from "../../components/ImageCarousel/ImageCarousel";
+import { Card } from "../../components/Card/Card"; // Importa el componente Card
+import { useMovies } from "../../hooks/useMovies"; // Importa el hook useMovies
 import image1 from "../../assets/image1.jpeg";
 import image2 from "../../assets/image2.jpeg";
 import image3 from "../../assets/image3.jpeg";
@@ -8,15 +10,25 @@ import image5 from "../../assets/image5.jpeg";
 
 export function HomePage() {
   const [images, setImages] = useState([]);
+  const {
+    nowPlayingMovies,
+    getNowPlayingMovies,
+  } = useMovies(); // Utiliza el hook useMovies
 
   useEffect(() => {
     // Fetch the list of images from the server (remember from firestore)
     setImages([image1, image2, image3, image4, image5]);
-  }, []);
+    getNowPlayingMovies(); // Obtiene las películas en cartelera
+  }, [getNowPlayingMovies]);
 
   return (
     <>
       <ImageCarousel images={images} />
+      <div>
+        {nowPlayingMovies.map((movie) => (
+          <Card key={movie.id} movie={movie} /> // Renderiza el componente Card para cada película
+        ))}
+      </div>
     </>
   );
 }
